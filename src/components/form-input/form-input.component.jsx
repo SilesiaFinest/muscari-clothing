3f1358e5943c61e177a7eb81d5b98945e46a4a44
Zbigniew Baranski <zb.baranski@icloud.com>
2020-04-13 20:01:44 +0200

    sign-in form styling & label transition

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 import "./form-input.styles.scss";
 
+// this is a custom component to make login labels go above input field when user start to type in them
+// label and input will always be togeter as 'group' the rest is passed as otherProps
+
 const FormInput = ({ handleChange, label, ...otherProps }) => (
+  // it passed props.value != 0 add class 'shrink' to the label
   <div className="group">
     <input className="form-input" onChange={handleChange} {...otherProps} />
-    {label ? <label></label> : null}
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? "shrink" : ""
+        } form-input-label `}
+      >
+        {label}
+      </label>
+    ) : null}
   </div>
 );
+
+export default FormInput;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import FormInput from "../form-input/form-input.component";
+
 import "./sign-in.styles.scss";
 
 class SignIn extends Component {
@@ -31,23 +33,22 @@ class SignIn extends Component {
         <span>Sign in with your email and password.</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
             required
           />
-          <lavel>Email</lavel>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
             required
           />
-          <label>Password</label>
-
           <input type="submit" value="Submit form" />
         </form>
       </div>
