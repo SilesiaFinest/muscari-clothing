d2de47130b50132356a7ee9ed2c11060b14447fc
Zbigniew Baranski <zb.baranski@icloud.com>
2020-04-23 00:43:14 +0200

    updated shopdata & cart.utils for grouping items

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,25 @@
+// utils is storing functions that nay be needed in multiple files
+
+//addItemToCart takes 2 arguments, current cartItems array and a new item to be added
+// it checks if new item exists in the array
+export const addItemToCart = (cartItems, cartItemtoAdd) => {
+  // if item exists in the array existingCartItem will be true (undefinded if not)
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemtoAdd.id
+  );
+
+  //if true cartItems.map is run and returns a new array. Returning a new array instead of
+  // updating the prevState to make sure the component is re-rendered
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemtoAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // above if block of code is run only when we are adding a new item, if not we use a below
+  // return statement which returns a new array with all existing cartItems and adding a
+  // new object equal to cartItemToAdd with added base quantity of 1
+  return [...cartItems, { ...cartItemtoAdd, quantity: 1 }];
+};
