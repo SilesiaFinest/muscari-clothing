20f0ea1e18763696aec170bb5e525a5da250001f
Zbigniew Baranski <zb.baranski@icloud.com>
2020-04-27 23:31:22 +0200

    cart-icon style added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,16 +8,22 @@ import { ReactComponent as CartSVG } from "../../assets/cart.svg";
 import "./cart-icon.styles.scss";
 
 // after use of connect&mapDispatchToProps {toggleCartHidden} is available as prop on component
-const CartIcon = ({ toggleCartHidden }) => (
+// itemCount from mapStateToProps is used to display number of items in the cart
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <CartSVG className="cart-svg" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+// using .reduce to get quantity of all items in the cart
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((sum, item) => sum + item.quantity, 0),
+});
 
 // dispatching toggle action
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
