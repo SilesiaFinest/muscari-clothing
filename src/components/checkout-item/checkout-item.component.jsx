0d55012c781f094d29c2f045a11d8fe9aca12d31
Zbigniew Baranski <zb.baranski@icloud.com>
2020-05-04 18:20:34 +0200

    checkout-item copmponent added

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./checkout-item.styles.scss";
+
+// passing whole cartItem as prop(so we can add or delete it from cart)
+// as well as destructuring other values we need
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img src={imageUrl} alt="item" />
+    </div>
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
+    <div className="remove-button">&#10005;</div>
+  </div>
+);
+
+export default CheckoutItem;
