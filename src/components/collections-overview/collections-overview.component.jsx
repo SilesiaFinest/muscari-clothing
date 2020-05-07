a079e00a58e7d13b8b8fe026a3f87e3b3c827357
Zbigniew Baranski <zb.baranski@icloud.com>
2020-05-07 21:04:15 +0200

    collections-overview added

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollections } from "../../redux/shop/shop.selector";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+import "./collections-overview.styles.scss";
+
+const CollectionsOverview = ({ collections }) => (
+  // destructuring parameters and using ES6 spread operator in collections.map and using ...
+  // to avoid passing key:values as CollectionPreview props like title={title}, routeName={routeName} etc.
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import { selectCollections } from "../../redux/shop/shop.selector";
-
-const ShopPage = ({ collections }) => (
-  // destructuring parameters and using ES6 spread operator in collections.map and using ...
-  // to avoid passing key:values as CollectionPreview props like title={title}, routeName={routeName} etc.
-
+const ShopPage = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateTopProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateTopProps)(ShopPage);
+export default ShopPage;
