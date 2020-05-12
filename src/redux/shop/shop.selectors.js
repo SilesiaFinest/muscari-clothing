d43ce07b74fa41820976f092d18883ab52b5d590
Zbigniew Baranski <zb.baranski@icloud.com>
2020-05-12 21:57:50 +0200

    optimized for data-normalization

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,16 +1,5 @@
 import { createSelector } from "reselect";
 
-// COLLECTION ID MAP is an object that maps the string value to respective ID
-// so that string value we get from the URL will be actual property
-// we using that to get dynamic value of the property to get correct ID
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -18,10 +7,9 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-// find collection.id matching the url parameter of COLLECTION_ID_MAP
+// data-normalization used
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
