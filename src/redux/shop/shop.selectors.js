3132d0073de82389fae79023499d942c6ab974ab
Zbigniew Baranski <zb.baranski@icloud.com>
2020-05-12 22:17:11 +0200

    collections-overview updated

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,7 +7,14 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-// data-normalization used
+// Object.keys gets the keys from the object and passes them in an array
+// so we can map over them to get an array of values of specific key
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+// data-normalization used so matching keys only (instead of array.find())
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [selectCollections],
