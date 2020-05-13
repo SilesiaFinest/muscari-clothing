d49c52921da02cc510a9413249fea1409f0f0889
Zbigniew Baranski <zb.baranski@icloud.com>
2020-05-13 18:46:30 +0200

    stripe checkout added

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_nnQJkUfQjurn26pmARTvNtv300SGqmfARf";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Muscari Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image={"https://i.ibb.co/DYcq8ZH/flower.png"}
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
